d162a02350bff8eea5dac82831e271e756258e8e
datdh <datdh@omegatheme.com>
2025-01-24 10:08:50 +0700

    feat: test deploy:v3

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World - dev"', () => {
-      expect(appController.getHello()).toBe('Hello World - dev 2');
+      expect(appController.getHello()).toBe('Hello World - dev 3');
     });
   });
 });
